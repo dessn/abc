ac5a64c4abaf9c21f17c17a86f8a86ac32abe43a
Samuel <samuelreay@gmail.com>
2016-03-14 13:21:58 +1000

    Adding the supernova rate prior as a beta function

--- dessn/toy/underlying.py
+++ dessn/toy/underlying.py
@@ -1,4 +1,9 @@
 from dessn.model.node import NodeUnderlying
+from scipy.stats import beta
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
 
 
 class Cosmology(NodeUnderlying):
@@ -28,10 +33,36 @@ class SupernovaIIDist(NodeUnderlying):
 class SupernovaRate(NodeUnderlying):
     def __init__(self):
         super(SupernovaRate, self).__init__("SN Rates", "sn_rate", "$r$")
+        self.b = beta(30, 2)
+
+    def plot_dist(self):
+        """ Plots the distribution for easy visualisation.
+        """
+        f = os.path.dirname(__file__) + "/../../plots/SupernovaRateDist.png"
+
+        xs = np.linspace(0, 1, 1000)
+        ys = self.b.pdf(xs)
+
+        fig, ax = plt.subplots(figsize=(6, 3))
+        ax.plot(xs, ys)
+        ax.set_xlabel("$r$")
+        ax.set_ylabel("$P(r)$")
+        fig.savefig(f, dpi=300, transparent=True, bbox_inches="tight")
 
     def get_log_prior(self, data):
-        return 1
+        r""" Here we model the prior on the supernova rate as a Beta function,
+        parametrised as :math:`{\rm Beta}(\alpha=30, \beta=2)`.
+
+        The probability density function is visualised below, where :math:`r` represents
+        the overall ratio of type Ia supernova over all supernova.
+
+        .. figure::     ../plots/SupernovaRateDist.png
+            :align:     center
+        """
+        r = data["sn_rate"]
+        return np.log(self.b.pdf(r))
 
 
-
-
+if __name__ == "__main__":
+    rate = SupernovaRate()
+    rate.plot_dist()
