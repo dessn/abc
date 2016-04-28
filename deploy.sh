a3515f342a45b05c09199623e36b768c4890b749
Samuel Hinton <samuelreay@gmail.com>
2016-04-28 18:23:31 +1000

    Second attempt at push

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,15 @@ if [ -n "$GITHUB_API_KEY" ]; then
     git config --global user.email "travis"
     git config --global user.name "travis"
     git init
-    git checkout -b gh-pages
     git add index.html
     git add .nojekyll
     git add doc
+    echo "Committing"
     git commit -m init
     # Make sure to make the output quiet, or else the API token will leak!
     # This works because the API key can replace your password.
-    git push -f -q https://$GITHUB_API_KEY@github.com/dessn/sn-doc gh-pages &2>/dev/null
+    echo "Pushing"
+    git push -f -q "https://${GITHUB_API_KEY2}@${GH_REF}" master:gh-pages
+    echo "Pushed"
 fi
 echo "Deploy script ending"
