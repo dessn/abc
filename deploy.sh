c1cbaf98767d73e51ad552721873bb3a2f2ac083
Samuel Hinton <samuelreay@gmail.com>
2016-04-28 17:48:53 +1000

    Updating deploy script to actually deploy

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,20 @@
 if [ -n "$GITHUB_API_KEY" ]; then
     echo "Github key found. Building documentation."
     cd "$TRAVIS_BUILD_DIR"/doc
-    echo `pwd`
-    echo `ls`
     make clean
     make rst
     make html
     cd "$TRAVIS_BUILD_DIR"
-    #git init
-    #git checkout -b gh-pages
-    #git add .
-    #git -c user.name='travis' -c user.email='travis' commit -m init
+    git init
+    git checkout -b gh-pages
+    git config --global user.email "travis"
+    git config --global user.email "travis"
+    git add index.html
+    git add .nojekyll
+    git add doc
+    git commit -m init
     # Make sure to make the output quiet, or else the API token will leak!
     # This works because the API key can replace your password.
-    #git push -f -q https://<me>:$GITHUB_API_KEY@github.com/dessn/abc gh-pages &2>/dev/null
-  fi
-echo "DOOM"
+    git push -f -q https://$GITHUB_API_KEY@github.com/dessn/sn-doc gh-pages &2>/dev/null
+fi
+echo "Deploy script ending"
