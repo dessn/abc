d5c107b2c09024e84a536bc8d760fb91f4a2c6ac
Samuel Hinton <samuelreay@gmail.com>
2017-01-14 15:40:21 +1000

    Adding skew for fun

---
+++ dessn/models/d_simple_stan/simple_skew/run.py
@@ -0,0 +1,13 @@
+import os
+from dessn.models.d_simple_stan.run import run, add_weight_to_chain
+
+if __name__ == "__main__":
+
+    file = os.path.abspath(__file__)
+    stan_model = os.path.dirname(file) + "/model.stan"
+    data = {
+        "data_source": "snana_dummy",
+        "n": 500
+    }
+    print("Running %s" % file)
+    run(data, stan_model, file, weight_function=add_weight_to_chain)
