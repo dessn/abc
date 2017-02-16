5ccdffd8a38b315b38f2ac456fd768e47f7f0e6a
Samuel Hinton <samuelreay@gmail.com>
2017-02-16 16:47:52 +1000

    Prepping dogmatix run

--- dessn/models/d_simple_stan/approx_skewnorm/run_diff.py
+++ dessn/models/d_simple_stan/approx_skewnorm/run_diff.py
@@ -12,14 +12,16 @@ if __name__ == "__main__":
     file = os.path.abspath(__file__)
     stan_model = os.path.dirname(file) + "/model.stan"
 
-    mB_mean, mB_width = get_approximate_mb_correction("snana")
-    print(mB_mean, mB_width)
+    mB_mean, mB_width, mB_alpha = get_approximate_mb_correction("snana")
+    print("Mean, width and alpha of selection function are ", mB_mean, mB_width, mB_alpha)
 
     data = {
         "mB_mean": mB_mean,
-        "mB_width": mB_width,
+        "mB_width2": mB_width**2,
+        "mB_alpha2": mB_alpha**2,
         "data_source": "snana_diff",
-        "n": 500
+        "n": 500,
+        "add_zs": 501
     }
     print("Running %s" % file)
     run(data, stan_model, file, weight_function=add_weight_to_chain)
