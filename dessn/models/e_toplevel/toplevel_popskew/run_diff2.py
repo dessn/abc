606f026a571174e81ca05da40b04e7fcf232a024
Samuel Hinton <samuelreay@gmail.com>
2017-03-14 13:52:42 +1000

    Stupid things

--- dessn/models/e_toplevel/toplevel_popskew/run_diff2.py
+++ dessn/models/e_toplevel/toplevel_popskew/run_diff2.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
         "mB_width2": mB_width**2,
         "mB_alpha2": mB_alpha**2,
         "data_source": "snana_diff2",
-        "n": 500
+        "n": 500,
     }
     print("Running %s" % file)
-    run(data, stan_model, file, weight_function=add_weight_to_chain)
+    # run(data, stan_model, file, weight_function=add_weight_to_chain)
+    run(data, stan_model, file)
