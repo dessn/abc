40c552ab11d25edf28d3f73b8bf5af694950cadc
Samuel Hinton <samuelreay@gmail.com>
2017-03-20 16:05:53 +1000

    Equialising number of sn

--- dessn/models/e_toplevel/toplevel_popskew/run_simple.py
+++ dessn/models/e_toplevel/toplevel_popskew/run_simple.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         "mB_width2": mB_width**2,
         "mB_alpha2": mB_alpha**2,
         "data_source": "simple",
-        "n": 300
+        "n": 500
     }
     print("Running %s" % file)
     run(data, stan_model, file)
