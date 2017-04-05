725059b3e809b80e27980114a89cd0f71640673e
Samuel Hinton <samuelreay@gmail.com>
2017-04-05 12:48:42 +1000

    Updating all plot

--- dessn/models/e_toplevel/toplevel_popskew/load_all.py
+++ dessn/models/e_toplevel/toplevel_popskew/load_all.py
@@ -12,15 +12,21 @@ if __name__ == "__main__":
 
     datas = ["snana_dummy", "snana_diff", "snana_diff2", "simple"]
     labels = [r"SNANA $\Omega_m=0.3$", r"SNANA $\Omega_m=0.2$", r"SNANA $\Omega_m=0.4$", "Simple model with mass"]
+    oms = [0.3, 0.2, 0.4, 0.3]
 
-    for data_source, label in zip(datas, labels):
+    for data_source, label, om in zip(datas, labels, oms):
 
         chain, posterior, t, p, f, l, w, ow = load_stan_from_folder(dir_name + "/stan_output_%s" % data_source,
                                                                 cut=False, merge=True, mod_weight=False)
-
+        print(list(chain.keys()))
+        chain[r"$\Delta \Omega_m$"] = chain['$\\Omega_m$'] - om
+        del chain['$\\Omega_m$']
         c.add_chain(chain, posterior=posterior, name=label)
 
     c.configure(shade=True)
-    c.plot(filename=dir_name + "/colour_all.png", truth=t, parameters=7, figsize=1.5)
+    parameters = [r"$\Delta \Omega_m$", '$\\alpha$', '$\\beta$', '$\\langle M_B \\rangle$', '$\\sigma_{\\rm m_B}$',
+                  '$\\sigma_{x_1}$', '$\\sigma_c$']
+    t[r"$\Delta \Omega_m$"] = 0
+    c.plot(filename=dir_name + "/colour_all.png", truth=t, parameters=parameters, figsize=1.5)
 
 
