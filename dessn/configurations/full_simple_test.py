8c47fb83c4d34338a9e84643de34f78ebcec3777
Samuel Hinton <samuelreay@gmail.com>
2017-04-18 16:34:39 +1000

    Turning mass and calibration back on

--- dessn/configurations/full_simple_test.py
+++ dessn/configurations/full_simple_test.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     model = FullModel(500, num_nodes=num_nodes)
     # Turn off mass and skewness for easy test
-    simulation = SimpleSimulation(alpha_c=0, mass=False, dscale=0.0, num_nodes=num_nodes)
+    simulation = SimpleSimulation(alpha_c=0, mass=True, dscale=0.08, num_nodes=num_nodes)
 
     fitter = Fitter(dir_name)
     fitter.set_models(model)
