3bbd7c8316090a0b3fb0d2622489650a5a0570f7
Samuel Hinton <samuelreay@gmail.com>
2017-04-19 17:32:56 +1000

    Logging the starting point to try and figure out why some runs fail.

--- dessn/configurations/full_simple_test.py
+++ dessn/configurations/full_simple_test.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         fitter.fit(file)
     else:
         from chainconsumer import ChainConsumer
-        chain, truth, weight, old_weight, posterior = fitter.load(max_deviation=2.0)
+        chain, truth, weight, old_weight, posterior = fitter.load()
         c = ChainConsumer()
         c.add_chain(chain, name="Stan")
         c.add_chain(chain, weights=weight, name="Corrected")
