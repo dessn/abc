7dea7e30a6cd8e5fec63c6764880f35d58ca628e
Samuel Hinton <samuelreay@gmail.com>
2017-05-10 11:50:37 +1000

    Converting to negative lcdf correction

--- dessn/configurations/approximate_snana_test.py
+++ dessn/configurations/approximate_snana_test.py
@@ -41,5 +41,5 @@ if __name__ == "__main__":
         parameters = ['$\\Omega_m$', '$\\alpha$', '$\\beta$', '$\\langle M_B \\rangle$',
                       '$\\sigma_{\\rm m_B}$', '$\\sigma_{x_1}$', '$\\sigma_c$',
                       '$\\delta(0)$', '$\\delta(\\infty)/\\delta(0)$']
-        # c.plot(filename=plot_filename, truth=truth, parameters=parameters)
-        c.plot(filename=plot_filename, truth=truth)
+        c.plot(filename=plot_filename, truth=truth, parameters=parameters)
+        # c.plot(filename=plot_filename, truth=truth)
