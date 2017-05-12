71263ba879bcc097d6830b11a6f8951eda277a41
Samuel Hinton <samuelreay@gmail.com>
2017-05-12 20:48:31 +1000

    Checking to make sure its not just redshift pops

--- dessn/configurations/full_snana_array_test.py
+++ dessn/configurations/full_snana_array_test.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     if not os.path.exists(plot_dir):
         os.makedirs(plot_dir)
 
-    num_nodes = 4
+    num_nodes = 1
 
     model = FullModelWithCorrection(500, num_nodes=num_nodes)
     # Turn off mass and skewness for easy test
