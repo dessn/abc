3dc363a5ee21c2360871cc49ad6a8817636f5dce
Samuel Hinton <samuelreay@gmail.com>
2017-06-09 16:23:29 -0700

    Updating API

--- dessn/configurations/approximate_simple_test.py
+++ dessn/configurations/approximate_simple_test.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         parameters = [r"$\Omega_m$", r"$\alpha$", r"$\beta$", r"$\langle M_B \rangle$",
                       r"$\delta M_B$", r"$\sigma_{\rm out}^{0}$",
                       r"$\delta(0)$", r"$\delta(\infty)/\delta(0)$"]
-        print(c.get_latex_table(transpose=True))
+        print(c.analysis.get_latex_table(transpose=True))
         c.plotter.plot(filename=plot_filename, truth=truth, parameters=parameters)
 
         c = ChainConsumer()
