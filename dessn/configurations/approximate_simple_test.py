613e087c2403f28728d8f3f5c4a5c1015a77f796
Samuel Hinton <samuelreay@gmail.com>
2017-06-28 17:32:03 -0700

    Multiple sims again

--- dessn/configurations/approximate_simple_test.py
+++ dessn/configurations/approximate_simple_test.py
@@ -21,9 +21,8 @@ if __name__ == "__main__":
 
     num_nodes = 4
     model = ApproximateModel(num_nodes=num_nodes)
-    #simulation = [SimpleSimulation(200, alpha_c=0, mass=True, dscale=0.08, num_nodes=num_nodes),
-    #              SimpleSimulation(200, alpha_c=0, mass=True, dscale=0.08, num_nodes=num_nodes, lowz=True)]
-    simulation = SimpleSimulation(1000, alpha_c=0, mass=True, dscale=0.08, num_nodes=num_nodes)
+    simulation = [SimpleSimulation(300, alpha_c=0, mass=True, dscale=0.08, num_nodes=num_nodes),
+                  SimpleSimulation(200, alpha_c=0, mass=True, dscale=0.08, num_nodes=num_nodes, lowz=True)]
     fitter = Fitter(dir_name)
     fitter.set_models(model)
     fitter.set_simulations(simulation)
