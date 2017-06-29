c8e2dbbc1db912972d6e29083acc56ec4fa0afc4
Samuel Hinton <samuelreay@gmail.com>
2017-06-29 10:59:36 -0700

    Increasing number of sims

--- dessn/configurations/stan_simple_test.py
+++ dessn/configurations/stan_simple_test.py
@@ -28,6 +28,8 @@ if __name__ == "__main__":
     fitter = Fitter(dir_name)
     fitter.set_models(model)
     fitter.set_simulations(simulation)
+    fitter.set_num_cosmologies(300)
+    fitter.set_num_walkers(1)
 
     h = socket.gethostname()
     if h != "smp-hk5pn72":  # The hostname of my laptop. Only will work for me, ha!
