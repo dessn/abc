cfe673c8a44fc71af7a2d30a589ff6f8a91e5632
Samuel Hinton <samuelreay@gmail.com>
2017-08-03 15:18:37 +1000

    Lets start testing snana without lowz

--- dessn/configurations/approximate_snana_test.py
+++ dessn/configurations/approximate_snana_test.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
 
     model = ApproximateModel()
     # Turn off mass and skewness for easy test
-    simulation = [SNANASimulationLowzGauss0p3(200), SNANASimulationGauss0p3(300)]
+    # simulation = [SNANASimulationLowzGauss0p3(200), SNANASimulationGauss0p3(300)]
+    simulation = SNANASimulationGauss0p3(500)
 
     lowzs, dess = simulation
     # print(lowzs.get_approximate_correction(plot=True))
