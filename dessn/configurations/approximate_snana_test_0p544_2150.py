48ec7dc6a5982bcc2b283b9d6ca191d77a2a0598
Samuel Hinton <samuelreay@gmail.com>
2017-09-26 19:11:32 +1000

    More permuations

---
+++ dessn/configurations/approximate_snana_test_0p544_2150.py
@@ -0,0 +1,49 @@
+import os
+import logging
+import socket
+from dessn.framework.fitter import Fitter
+from dessn.framework.models.approx_model import ApproximateModel
+from dessn.framework.simulations.snana import SNANASimulationGauss0p3
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    plot_dir = os.path.dirname(os.path.abspath(__file__)) + "/plots/%s/" % os.path.basename(__file__)[:-3]
+    dir_name = plot_dir + "output/"
+    plot_filename = plot_dir + os.path.basename(__file__)[:-3] + ".png"
+    file = os.path.abspath(__file__)
+    print(dir_name)
+
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    model = ApproximateModel()
+    simulation = SNANASimulationGauss0p3(1000, manual_selection=[21.5, 0.544, None, 1.0])
+    # print("CORRECTION IS ", simulation.get_approximate_correction(plot=True))
+
+    fitter = Fitter(dir_name)
+    fitter.set_models(model)
+    fitter.set_simulations(simulation)
+    fitter.set_num_cosmologies(300)
+    fitter.set_num_walkers(1)
+    fitter.set_max_steps(3000)
+
+    h = socket.gethostname()
+    if h != "smp-hk5pn72":  # The hostname of my laptop. Only will work for me, ha!
+        fitter.fit(file)
+    else:
+        from chainconsumer import ChainConsumer
+        m, s, chain, truth, weight, old_weight, posterior = fitter.load()
+        c = ChainConsumer()
+        c.add_chain(chain, weights=weight, posterior=posterior, name="Approx")
+        c.configure(spacing=1.0)
+
+        parameters = [r"$\Omega_m$", r"$\alpha$", r"$\beta$", r"$\langle M_B \rangle$"]
+        print(c.analysis.get_latex_table(transpose=True))
+        c.plotter.plot(filename=plot_filename, truth=truth, parameters=parameters)
+        print("Plotting distributions")
+        c = ChainConsumer()
+        c.add_chain(chain, weights=weight, posterior=posterior, name="Approx")
+        c.configure(label_font_size=10, tick_font_size=10, diagonal_tick_labels=False)
+        c.plotter.plot_distributions(filename=plot_filename.replace(".png", "_dist.png"), truth=truth, col_wrap=8)
+        with open(plot_filename.replace(".png", ".txt"), 'w') as f:
+            f.write(c.analysis.get_latex_table(parameters=parameters))
