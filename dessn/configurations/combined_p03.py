38ae503199f2f8a7001e637af711d66f2120fc23
Samuel Hinton <samuelreay@gmail.com>
2017-10-04 15:09:49 +1000

    Combined test

---
+++ dessn/configurations/combined_p03.py
@@ -0,0 +1,53 @@
+import os
+import logging
+import socket
+from dessn.framework.fitter import Fitter
+from dessn.framework.models.approx_model import ApproximateModel
+from dessn.framework.simulations.snana_bulk import SNANACombinedBulk
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    plot_dir = os.path.dirname(os.path.abspath(__file__)) + "/plots/%s/" % os.path.basename(__file__)[:-3]
+    dir_name = plot_dir + "output/"
+    pfn = plot_dir + os.path.basename(__file__)[:-3]
+
+    file = os.path.abspath(__file__)
+    print(dir_name)
+
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    model = ApproximateModel()
+    # Turn off mass and skewness for easy test
+    simulation = [SNANACombinedBulk(500, ["SHINTON_LOWZ_MATRIX_G10_SKEWC_SKEWX1", "SHINTON_LOWZ_MATRIX_C11_SKEWC_SKEWX1"],
+                                    "CombinedLowZ", manual_selection=[13.70+0.3, 1.4, 3.8, 0.2], num_calib=58),
+                  SNANACombinedBulk(500, ["SHINTON_DES_MATRIX_G10_SKEWC_SKEWX1", "SHINTON_DES_MATRIX_C11_SKEWC_SKEWX1"],
+                                    "CombinedDES", manual_selection=[22.1+0.3, 0.7, None, 1.0], num_calib=22)]
+
+    fitter = Fitter(dir_name)
+    fitter.set_models(model)
+    fitter.set_simulations(simulation)
+    fitter.set_num_cosmologies(10)
+    fitter.set_num_walkers(5)
+    fitter.set_max_steps(3000)
+
+    h = socket.gethostname()
+    if h != "smp-hk5pn72":  # The hostname of my laptop. Only will work for me, ha!
+        fitter.fit(file)
+    else:
+        from chainconsumer import ChainConsumer
+        m, s, chain, truth, weight, old_weight, posterior = fitter.load()
+        c = ChainConsumer()
+        c.add_chain(chain, weights=weight, posterior=posterior, name="Approx")
+        c.configure(spacing=1.0)
+
+        parameters = [r"$\Omega_m$", r"$\alpha$", r"$\beta$", r"$\langle M_B \rangle$"]
+        print(c.analysis.get_latex_table(transpose=True))
+        c.plotter.plot(filename=pfn + ".png", truth=truth, parameters=parameters)
+        print("Plotting distributions")
+        c = ChainConsumer()
+        c.add_chain(chain, weights=weight, posterior=posterior, name="Approx")
+        c.configure(label_font_size=10, tick_font_size=10, diagonal_tick_labels=False)
+        c.plotter.plot_distributions(filename=pfn + "_dist.png", truth=truth, col_wrap=8)
+
