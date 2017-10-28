e500f5129f64ce0d5b5ab8b46dffe70cc6c97f88
Samuel Hinton <samuelreay@gmail.com>
2017-10-29 01:12:15 +1000

    Bug fix

--- dessn/framework/simulations/selection_effects.py
+++ dessn/framework/simulations/selection_effects.py
@@ -4,6 +4,6 @@ def des_sel():
 
 
 def lowz_sel():
-    return [15.5, 1.0, None, 1.0]  # As a cdf
-    return [13.75, 1.45+0.25, 7.5, 0.3]  # Skew normal, but high skewness
+    # return [15.5, 1.0, None, 1.0]  # As a cdf
     # return [13.70, 1.4+0.25, 3.8, 0.2]  # Original selection effect
+    return [13.75, 1.45+0.25, 7.5, 0.3]  # Skew normal, but high skewness
