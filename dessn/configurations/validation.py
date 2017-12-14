192b331ba6aa6c923cf0e60df333dbe3bb13135d
Samuel Hinton <samuelreay@gmail.com>
2017-12-14 11:44:19 +1100

    Increasing size of validation sim

--- dessn/configurations/validation.py
+++ dessn/configurations/validation.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     fitter.set_simulations(*simulations)
     fitter.set_num_cosmologies(200)
     fitter.set_max_steps(3000)
-    fitter.set_num_walkers(1)
+    fitter.set_num_walkers(2)
 
     h = socket.gethostname()
     if h != "smp-hk5pn72":  # The hostname of my laptop. Only will work for me, ha!
