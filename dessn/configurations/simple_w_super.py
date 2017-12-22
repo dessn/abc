753630520cf3a3e42340593cb0756ca8590f46e1
Samuel Hinton <samuelreay@gmail.com>
2017-12-23 01:18:44 +1000

    Too many!

--- dessn/configurations/simple_w_super.py
+++ dessn/configurations/simple_w_super.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     fitter = Fitter(dir_name)
     fitter.set_models(model)
     fitter.set_simulations(simulation)
-    ncosmo = 2000
+    ncosmo = 500
     fitter.set_num_cosmologies(ncosmo)
     fitter.set_num_walkers(1)
     fitter.set_max_steps(2000)
