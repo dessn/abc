f21430a1ace684be11e7f40ce7b80fb96e99b301
Samuel Hinton <samuelreay@gmail.com>
2018-01-08 17:53:13 +1000

    Adding data fixing of sigma_c, rerunning

--- dessn/configurations/simple_w_array.py
+++ dessn/configurations/simple_w_array.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         os.makedirs(dir_name)
 
     models = [
-        # ApproximateModelW(prior=True, frac_alpha=0.0,  frac_shift=0.0, frac_shift2=0.0),
+        ApproximateModelW(prior=True, frac_alpha=0.0,  frac_shift=0.0, frac_shift2=0.0),
         ApproximateModelW(prior=True, frac_alpha=0.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.1),
         # ApproximateModelW(prior=True, frac_alpha=-1.0, frac_shift=0.0, frac_shift2=0.0),
         # ApproximateModelW(prior=True, frac_alpha=-1.0, frac_shift=1.0, frac_shift2=1.0),
