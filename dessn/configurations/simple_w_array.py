a89582c4a384f236e47b7b3ef291e73014de9332
Samuel Hinton <samuelreay@gmail.com>
2018-01-10 22:01:09 +1000

    Full sized test

--- dessn/configurations/simple_w_array.py
+++ dessn/configurations/simple_w_array.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     models = [
         ApproximateModelW(prior=True, frac_alpha=0.0,  frac_shift=0.0, frac_shift2=0.0),
         # ApproximateModelW(prior=True, frac_alpha=0.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07),
-        # ApproximateModelW(prior=True, frac_alpha=-1.0, frac_shift=0.0, frac_shift2=0.0),
+        ApproximateModelW(prior=True, frac_alpha=-1.0, frac_shift=0.0, frac_shift2=0.0),
         ApproximateModelW(prior=True, frac_alpha=-1.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07),
 
     ]
