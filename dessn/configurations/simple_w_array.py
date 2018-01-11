e0c9b592ece284241b3a6cd6cb695aeb80ffd71f
Samuel Hinton <samuelreay@gmail.com>
2018-01-11 10:43:58 +1000

    Beta contrib test

--- dessn/configurations/simple_w_array.py
+++ dessn/configurations/simple_w_array.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
         ApproximateModelW(prior=True, frac_alpha=0.0,  frac_shift=0.0, frac_shift2=0.0),
         # ApproximateModelW(prior=True, frac_alpha=0.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07),
         # ApproximateModelW(prior=True, frac_alpha=-1.0, frac_shift=0.0, frac_shift2=0.0),
-        ApproximateModelW(prior=True, frac_alpha=-1.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07, beta_contrip=0.1),
-        ApproximateModelW(prior=True, frac_alpha=-1.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07, beta_contrip=0.05),
-        ApproximateModelW(prior=True, frac_alpha=-1.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07, beta_contrip=0.15),
+        ApproximateModelW(prior=True, frac_alpha=-1.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07, beta_contrib=0.1),
+        ApproximateModelW(prior=True, frac_alpha=-1.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07, beta_contrib=0.05),
+        ApproximateModelW(prior=True, frac_alpha=-1.0,  frac_shift=1.0, frac_shift2=1.0, fixed_sigma_c=0.07, beta_contrib=0.15),
 
     ]
     simulations = [
