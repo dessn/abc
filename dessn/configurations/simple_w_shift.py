c39a19000b821013e744d0f196800066ff2e3322
Samuel Hinton <samuelreay@gmail.com>
2018-02-22 22:33:03 +1000

    Checking to see if plots look better with no redshift dependency

--- dessn/configurations/simple_w_shift.py
+++ dessn/configurations/simple_w_shift.py
@@ -71,44 +71,13 @@ if __name__ == "__main__":
         c1.configure(spacing=1.0, diagonal_tick_labels=False, sigma2d=False, linestyles=ls,
                      colors=cs, max_ticks=4, linewidths=1.1)
 
-        # print("Adding individual realisations")
-        # res = fitter.load(split_cosmo=True)
-        # for i, (m, s, ci, chain, truth, weight, old_weight, posterior) in enumerate(res):
-        #     c2.add_chain(chain, weights=weight, posterior=posterior, name="Realisation %d" % i)
-        #
-        # c2.configure(spacing=1.0, diagonal_tick_labels=False, sigma2d=False, statistics="mean")
-
-        # import numpy as np
-        # ws = [chain.chain[:, chain.parameters.index("$w$")] for chain in c2.chains]
-        # means = [np.mean(w) for w in ws]
-        # stds = [np.std(w) for w in ws]
-        # mean_mean = np.average(means, weights=1 / (np.array(stds) ** 2))
-        # mean_std = np.mean(stds)
-        # bias = (mean_mean + 1) / mean_std
-        #
-        # save = "%10s %0.4f(%0.4f) %0.4f %0.4f (abs(w+1) %0.4f)\n" % ("Simple", mean_mean, mean_std, np.std(means), bias, np.abs(mean_mean + 1))
-        # print(save)
-        # with open(pfn + "_test.txt", "w") as f:
-        #     f.write(save)
-
-        # print("Saving table")
-        # print(c1.analysis.get_latex_table(transpose=True))
-        # with open(pfn + "_cosmo_params.txt", 'w') as f:
-        #     f.write(c1.analysis.get_latex_table(parameters=parameters))
-        #
-        # print("Plotting cosmology")
-        # c1.plotter.plot(filename=[pfn + "_cosmo.png", pfn + "_cosmo.pdf"], truth=truth, parameters=parameters,
-        #                 figsize="column")
-        #
-        # print("Plotting summaries")
-        # c1.plotter.plot_summary(filename=[pfn + "_summary.png", pfn + "_summary.pdf"], truth=truth, parameters=parameters, errorbar=True,
-        #                         extra_parameter_spacing=1.0)
+        c1.plotter.plot(filename=pfn + "_small.png", truth=truth, parameters=2)
 
         print("Plotting distributions")
         fig = c1.plotter.plot_distributions(truth=truth, col_wrap=5, parameters=red_params)
         ax = fig.get_axes()
-        ax[2].set_ylim(0, 10)
-        ax[3].set_ylim(0, 10)
+        ax[10].set_ylim(0, 10)
+        ax[11].set_ylim(0, 10)
         filenames = [pfn + "_dist.png", pfn + "_dist.pdf"]
         for f in filenames:
             c1.plotter._save_fig(fig, f, 300)
