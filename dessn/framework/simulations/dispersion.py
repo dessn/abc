a42e8396340c77cb6a5f02e216da7cead7ba4fc9
Samuel Hinton <samuelreay@gmail.com>
2018-09-10 09:55:14 +1000

    Turning dispersion back on, lets find some older data and see what we get

--- dessn/framework/simulations/dispersion.py
+++ dessn/framework/simulations/dispersion.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import binned_statistic
 
 
 def get_data(des=True, model="G10"):
@@ -29,6 +30,25 @@ if __name__ == "__main__":
 
     g10_diff = g10["obs"] - g10["sim"]
     c11_diff = c11["obs"] - c11["sim"]
+
+    bine = 10
+    g10_d_mb, bine, _ = binned_statistic(g10["z"], g10_diff[:, 0], bins=bine)
+    g10_d_x1, bine, _ = binned_statistic(g10["z"], g10_diff[:, 1], bins=bine)
+    g10_d_c, bine, _  = binned_statistic(g10["z"], g10_diff[:, 2], bins=bine)
+    c11_d_mb, bine, _ = binned_statistic(c11["z"], c11_diff[:, 0], bins=bine)
+    c11_d_x1, bine, _ = binned_statistic(c11["z"], c11_diff[:, 1], bins=bine)
+    c11_d_c, bine, _  = binned_statistic(c11["z"], c11_diff[:, 2], bins=bine)
+    binc = 0.5 * (bine[:-1] + bine[1:])
+    print("Bias amount (100ths of a mag)")
+    for i, bc in enumerate(binc):
+        print("%5.3f | %6.3f %6.3f | %6.3f %6.3f | %6.3f %6.3f" % (bc, 100 * g10_d_mb[i], 100 * c11_d_mb[i], 100 * 0.1 * g10_d_x1[i], 100 * 0.1 * c11_d_x1[i], 100 * 3 * g10_d_c[i], 100 * 3 * c11_d_c[i]))
+
+    # import matplotlib.pyplot as plt
+    # plt.scatter(c11["z"], c11["c"], s=1, c='r', alpha=0.5)
+    # plt.scatter(c11["z"], c11["sim_c"], s=1, c='k', alpha=0.5)
+    # plt.hist(c11["c"], 50, histtype="step")
+    # plt.hist(c11["sim_c"], 50, histtype="step")
+    # plt.show()
 
     print("Diff")
     print(np.mean(g10_diff, axis=0))
