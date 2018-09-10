6bb0b552c9968a1533dfb55522f04881977e9e68
Samuel Hinton <samuelreay@gmail.com>
2018-09-10 10:09:49 +1000

    v5 fits

--- dessn/configurations/bulk_tests.py
+++ dessn/configurations/bulk_tests.py
@@ -31,8 +31,8 @@ if __name__ == "__main__":
     sim = False
     disp = False
     simulations = [
-        [SNANASimulation(ndes, "DES3YR_DES_BULK_G10_SKEW", type=None, use_sim=sim, add_disp=disp), SNANASimulation(nlowz, "DES3YR_LOWZ_BULK_G10_SKEW", use_sim=sim, add_disp=disp)],
-        [SNANASimulation(ndes, "DES3YR_DES_BULK_C11_SKEW", type=None, use_sim=sim, add_disp=disp), SNANASimulation(nlowz, "DES3YR_LOWZ_BULK_C11_SKEW", use_sim=sim, add_disp=disp)],
+        [SNANASimulation(ndes, "DES3YR_DES_BULK_G10_SKEW_v5", use_sim=sim, add_disp=disp), SNANASimulation(nlowz, "DES3YR_LOWZ_BULK_G10_SKEW_v5", use_sim=sim, add_disp=disp)],
+        [SNANASimulation(ndes, "DES3YR_DES_BULK_C11_SKEW_v5", use_sim=sim, add_disp=disp), SNANASimulation(nlowz, "DES3YR_LOWZ_BULK_C11_SKEW_v5", use_sim=sim, add_disp=disp)],
     ]
     fitter = Fitter(dir_name)
 
