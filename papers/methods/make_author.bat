bbb0c6c3a93a9f66ed0ee2d13cdd9f46caffd1c4
Samuel Hinton <samuelreay@gmail.com>
2018-11-06 09:13:31 +1000

    Updating authorlist and removing line numbers

--- papers/methods/make_author.bat
+++ papers/methods/make_author.bat
@@ -1,1 +1,1 @@
-python C:\Anaconda3\Lib\site-packages\mkauthlist\mkauthlist.py -f --doc -j emulateapj --sort --sort-builder --aux order.csv author_list.csv author_list.tex
+python C:\Anaconda3\Lib\site-packages\mkauthlist\mkauthlist.py -j emulateapj --sort-nonbuilder -a order.csv author_list.csv author_list.tex
